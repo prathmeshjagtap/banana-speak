f0a9c9486d7b3275c476503e27fed06e48efadac
prathmesh <prathmeshjagtap405@gmail.com>
2020-12-12 14:47:21 +0530

    feat - addded text area

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// console.log("heloo javascript");
-
-// var name = prompt("Please provide your name");
-// var welcomeMessage = "Welcome " + name;
-// alert(welcomeMessage);
-
 var transalte = document.querySelector("#button_translate");
+var textInput = document.querySelector("#text-input");
 
-transalte.addEventListener("click", function clickHandler() {
-  console.log("clicked");
-});
+function clickHandler() {
+  console.log("Clicked");
+  console.log(textInput.value);
+}
+
+transalte.addEventListener("click", clickHandler);
