a7869c769878a046a2d1085a8b47da041925d7ec
prathmesh <prathmeshjagtap405@gmail.com>
2020-12-12 14:38:25 +0530

    feat - added click button and function

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // console.log("heloo javascript");
 
-var name = prompt("Please provide your name");
-var welcomeMessage = "Welcome " + name;
-alert(welcomeMessage);
+// var name = prompt("Please provide your name");
+// var welcomeMessage = "Welcome " + name;
+// alert(welcomeMessage);
+
+var transalte = document.querySelector("#button_translate");
+
+transalte.addEventListener("click", function clickHandler() {
+  console.log("clicked");
+});
